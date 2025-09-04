378e77c893c93b575ca51f4b701ad6874de6f340
Jost2167 <johnstiven2000@gmail.com>
2025-09-03 19:47:05 -0500

    end

--- EntryLog.Business/Cryptography/Argon2PasswordHashOptions.cs
+++ EntryLog.Business/Cryptography/Argon2PasswordHashOptions.cs
@@ -3,9 +3,9 @@ namespace EntryLog.Business.Cryptography;
 // Clase para configurar las opciones de Argon2
 public class Argon2PasswordHashOptions
 {
-    public int DegreeOfParallelism { get; set; }
-    public int MemorySize { get; set; }
-    public int Iterations { get; set; }
-    public int SaltSize { get; set; }
-    public int HashSize { get; set; }
+    public int DegreeOfParallelism { get; init; }
+    public int MemorySize { get; init; }
+    public int Iterations { get; init; }
+    public int SaltSize { get; init; }
+    public int HashSize { get; init; }
 }
